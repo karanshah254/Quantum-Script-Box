bce756ce631b09e813a9c844a368bcae25fdc4dc
karanshah254 <karankshah2504@gmail.com>
2024-01-21 16:50:51 +0530

    new commit

---
+++ 04-funtions/Good-Scoping/scoping.js
@@ -0,0 +1,10 @@
+let lightWaves = 'Moonlight';
+
+function check(lightWaves) {
+    if (lightWaves === 'Arctic lights') {
+        console.log(lightWaves);
+    }
+}
+
+check('Arctic lights');
+console.log(lightWaves);
---
+++ 04-funtions/global-local-scope/program.js
@@ -0,0 +1,10 @@
+const satellite = "The Moon";
+const galaxy = "The Milky Way";
+const stars = "North Star";
+
+function callMyNightSky() {
+    return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+
+console.log(callMyNightSky());
+// Night Sky: The Moon, North Star, and The Milky Way
