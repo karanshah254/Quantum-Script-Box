0c360059f3a2c54fb6c519e24463587350f633aa
karanshah254 <karankshah2504@gmail.com>
2024-01-29 02:35:37 +0530

    new commit

---
+++ 03_JS/Array_in_JS/array-methods/remove-element.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5];
+
+console.log('The array after pop is', array);
+console.log('Pop element is', array.pop());
---
+++ 04-funtions/for-each-callback/program.js
@@ -0,0 +1,8 @@
+let arr = ["gujarat", "delhi", "uttar pradesh", "maharastra"];
+
+arr.forEach((val) => {
+    console.log(val.toUpperCase());
+})
+
+// forEach is called callback function 
+// callback function is function passed as an argument to another function
