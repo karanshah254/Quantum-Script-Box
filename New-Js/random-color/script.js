b4c0589b864e33fb0fbb2e32b795a1d2bfea08d1
karanshah254 <karankshah2504@gmail.com>
2024-05-21 01:00:25 +0530

    watch yourselves

---
+++ New-Js/random-color/script.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector("button");
+let h1 = document.querySelector("h1");
+let div = document.querySelector("div");
+
+btn.addEventListener("click", () => {
+    h1.innerText = getRandomColor();
+    div.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let color = `rgb(${red}, ${green}, ${blue})`;
+
+    return color;
+}
